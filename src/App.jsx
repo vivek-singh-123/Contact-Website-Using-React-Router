b235bba355b3877c1777031f2de305915b2883e7
Vivek Singh <singhvivek357480@gmail.com>
2024-09-12 04:46:27 +0530

    project is in intial phase

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='text-black text-4xl bg-pink-400'>contact website</div>
+    </>
+  )
+}
+
+export default App
